75b69f26472f9dfb487325482e10b3d52a16c664
Alex <a.laroy@students.ephec.be>
2024-12-12 00:16:50 +0100

    'WIP'

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -15,8 +15,6 @@ export async function POST(request) {
     try {
         const body = await request.json();
         let { email, password } = body;
-        email = 'dummy@example.com';
-        password = 'dummy_password';
 
         if (!email || !password) {
             return NextResponse.json({ error: 'Email et mot de passe requis' }, { status: 400 });
